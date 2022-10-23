d0a6a8961b467230aabdbd67036d3096fdc45e6c
Danny Sag <molokov@gmail.com>
2022-10-23 15:24:15 +1030

    Updating to work with Django 4.x/Mezzanine 6.0/Cartridge 1.3.4. Updating version number

--- cartridge_pinpayments/forms.py
+++ cartridge_pinpayments/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.utils.html import format_html
 from django.forms.utils import flatatt
 
